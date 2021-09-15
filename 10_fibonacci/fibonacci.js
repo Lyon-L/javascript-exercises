1b451e199bce674ab6ce544ab8ea9df166cb4133
Leandro <leandrovogt.translator@gmail.com>
2021-09-15 16:43:13 -0300

    solved fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,23 @@
-const fibonacci = function() {
+const fibonacci = function(fib) {
+    let sum = 0; 
+    let x2 = 0;
+    let x1 = 1;
+    num = parseInt(fib);
+    if(num < 0) {
+        return "OOPS";
+    } else if(num === 0) {
+        return 0;
+    } else if (num === 1 || num === 2) {
+        return 1;
+    } else {
+        for(let i = 1; i < num; i++) {
+            sum = x1 + x2;
+            x2 = x1;
+          	x1 = sum;
+        }
 
+        return sum;
+    }
 };
 
 module.exports = fibonacci;
